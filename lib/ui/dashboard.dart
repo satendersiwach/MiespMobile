d948092507f883844e0f952156f920acedd7e0e8
Ayush Pratik <ayush.kishul@gmail.com>
2024-08-09 00:24:10 +0530

    Improved Dashboard UI

--- lib/ui/dashboard.dart
+++ lib/ui/dashboard.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:scanner/theme/custom_colors.dart';
+import 'package:scanner/theme/custom_text_widgets.dart';
 import 'package:scanner/theme/elements_screen.dart';
 import 'package:scanner/ui/components/custom_drawer.dart';
-import 'package:flutter/material.dart';
 
 class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
@@ -10,14 +13,50 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+  PackageInfo? packageInfo;
+
+  @override
+  void initState() {
+    super.initState();
+    setVersion();
+  }
+
+  setVersion() async {
+    packageInfo = await PackageInfo.fromPlatform();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return screenWithAppBar(
         title: 'Scanner App',
         drawer: CustomDrawer(),
-        body: const SingleChildScrollView(
+        body: SingleChildScrollView(
           child: Column(
-            children: [],
+            children: [
+              if (packageInfo != null) ...[
+                Padding(
+                  padding: const EdgeInsets.only(top: 20.0, bottom: 8),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      getPoppinsText(
+                          text: 'Current app version',
+                          color: appPrimary,
+                          fontWeight: FontWeight.bold),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 4),
+                        child: getPoppinsText(
+                            text:
+                                ' - ${packageInfo?.version ?? ''}(${packageInfo?.buildNumber ?? ''})',
+                            color: Colors.red,
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ],
           ),
         ));
   }
